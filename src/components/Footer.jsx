8a7c1a46924c50e9c845b312eb52e3cbc1c52038
Laia Navalon <laianavalon@gmail.com>
2024-06-12 19:23:33 +0200

    Initialise Header and Footer components with basic content

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,19 @@
-function Footer() {}
+function Footer() {
+  return (
+    <div>
+      <hr />
+      <div>
+        <div>web menu</div>
+        <div>
+          <a href="">Instagram</a>
+          <a href="">Pinterest</a>
+          <a href="mailto:studio@gadriana.co"></a>
+        </div>
+      </div>
+      <hr />
+      <p>©2024 GADRIANA.</p>
+    </div>
+  );
+}
 
 export default Footer;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,23 @@
-function Header() {}
+import { NavLink } from "react-router-dom";
+
+function Header() {
+  return (
+    // TODO: install Tailwind
+    <div>
+      <div>
+        {" "}
+        {/* TODO: Add logo img */}
+        {/* <img src="" alt="" /> */}
+        <div>
+          <NavLink to="/about">About</NavLink>
+          <NavLink to="/services">Services</NavLink>
+          <NavLink to="/contact">Contact</NavLink>
+          <a href="">Instagram</a>
+        </div>
+      </div>
+      <hr />
+    </div>
+  );
+}
 
 export default Header;
