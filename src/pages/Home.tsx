65c2149977e93382ba1ea32a94a9896ecfd0605b
Laia Navalon <laianavalon@gmail.com>
2024-07-04 17:46:11 +0200

    Add Y margin to all pages so content is not hidden behind Header.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ function Home() {
   return (
     <div className="w-full">
       <div className="max-w-7xl flex justify-between items-center mx-auto">
-        <div className="flex-col">
+        <div className="flex-col my-32">
           <h1 className="my-12 text-4xl w-2/3 leading-relaxed">
             We're a Barcelona-based creative studio for beauty + wellness
             brands.
--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -2,7 +2,7 @@ function Services() {
   return (
     <div className="w-full">
       <div className="max-w-7xl flex justify-between items-center mx-auto">
-        <div className="flex-col">
+        <div className="flex-col my-32">
           <h1>This is Services.tsx</h1>
         </div>
       </div>
