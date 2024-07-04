007000fa6745336463c1ede9d938704ee6598aac
Laia Navalon <laianavalon@gmail.com>
2024-07-04 16:30:54 +0200

    Start creating Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,19 @@
+function Footer() {
+  return (
+    <div>
+      <hr />
+      <div>
+        <div>web menu</div>
+        <div>
+          <a href="">Instagram</a>
+          <a href="">Pinterest</a>
+          <a href="mailto:studio@gadriana.co"></a>
+        </div>
+      </div>
+      <hr />
+      <p>©2024 GADRIANA.</p>
+    </div>
+  );
+}
+
+export default Footer;
