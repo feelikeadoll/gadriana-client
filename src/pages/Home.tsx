d48e365c7fd9169efb8bc05085dd13d7c0bc9289
Laia Navalon <laianavalon@gmail.com>
2024-07-04 16:25:51 +0200

    Create initial pages and components

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+function Home() {
+  return (
+    <div>
+      <h1>
+        We're a Barcelona-based creative studio for beauty + wellness brands.{" "}
+      </h1>
+      <div>images flex grid</div>
+    </div>
+  );
+}
+
+export default Home;
