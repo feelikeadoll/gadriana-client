caec7435aec6d1099de5dfbdd711e1f0d2877ac1
Laia Navalon <laianavalon@gmail.com>
2024-07-04 17:20:39 +0200

    Style Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,24 +1,60 @@
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 import Logo from "../assets/gadriana-logo.png";
 
 function Header() {
   return (
     <div className="w-full">
-      <div>
-        <img
-          src={Logo}
-          alt="Gadriana Creative Product Photography Studio located Barcelona"
-          className=""
-        />
-        <div>
-          <NavLink to="/about">About</NavLink>
-          <NavLink to="/services">Services</NavLink>
-          <NavLink to="">Contact</NavLink>
-          <a href="">Instagram</a>
+      <div className="max-w-7xl flex justify-between items-center mx-auto">
+        <div className="my-6 w-full flex items-center justify-between">
+          <Link to="/">
+            <img
+              src={Logo}
+              alt="Gadriana Creative Product Photography Studio located Barcelona"
+              className="w-56"
+            />
+          </Link>
+          <div className="flex items-center">
+            <NavLink to="/about" className="ml-10 text-lg">
+              About
+            </NavLink>
+            <NavLink to="/services" className="ml-10 text-lg">
+              Services
+            </NavLink>
+            <NavLink to="" className="ml-10 text-lg">
+              Contact
+            </NavLink>
+            <a
+              href="https://www.instagram.com/gadrianastudio/"
+              className="ml-10 text-lg"
+            >
+              <svg
+                fill="#000000"
+                width="30px"
+                height="30px"
+                viewBox="0 0 64 64"
+                xmlns="http://www.w3.org/2000/svg"
+                stroke="#000000"
+                stroke-width="0.64"
+              >
+                <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                <g
+                  id="SVGRepo_tracerCarrier"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                ></g>
+                <g id="SVGRepo_iconCarrier">
+                  <title></title>
+                  <path d="M44,57H20A13,13,0,0,1,7,44V20A13,13,0,0,1,20,7H44A13,13,0,0,1,57,20V44A13,13,0,0,1,44,57ZM20,9A11,11,0,0,0,9,20V44A11,11,0,0,0,20,55H44A11,11,0,0,0,55,44V20A11,11,0,0,0,44,9Z"></path>
+                  <path d="M32,43.67A11.67,11.67,0,1,1,43.67,32,11.68,11.68,0,0,1,32,43.67Zm0-21.33A9.67,9.67,0,1,0,41.67,32,9.68,9.68,0,0,0,32,22.33Z"></path>
+                  <path d="M44.5,21A3.5,3.5,0,1,1,48,17.5,3.5,3.5,0,0,1,44.5,21Zm0-5A1.5,1.5,0,1,0,46,17.5,1.5,1.5,0,0,0,44.5,16Z"></path>
+                </g>
+              </svg>
+            </a>
+          </div>
         </div>
       </div>
-      <hr />
+      <hr className="border-black border-1" />
     </div>
   );
 }
