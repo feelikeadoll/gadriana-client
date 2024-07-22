77a353272dfa50dfa676a65e7964efd8dd235a23
Laia Navalon <laianavalon@gmail.com>
2024-07-22 12:54:31 +0200

    Add max height attributes to Services header img for responsiveness + Adjust font size and Y margins for better experience in smaller screens

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -11,14 +11,14 @@ function Services() {
           <img
             src={ServicesTitle}
             alt="Gadriana Creative Studio - About"
-            className="my-8 max-h-28"
+            className="my-8 max-h-16 sm:max-h-20 md:max-h-28"
           />
           <section
             id="photograpy"
-            className="w-full mt-20 md:mt-28 mb-20 flex flex-col md:flex-row justify-between"
+            className="w-full mt-16 md:mt-28 mb-20 flex flex-col md:flex-row justify-between"
           >
-            <h3 className="text-5xl md:w-1/2">Photography</h3>
-            <p className="text-xl mt-12 md:mt-0 md:w-1/2">
+            <h3 className="text-4xl md:text-5xl md:w-1/2">Photography</h3>
+            <p className="text-lg md:text-xl mt-8 md:mt-12 md:mt-0 md:w-1/2">
               We offer professional photography services to help you capture the
               perfect image for your product. From pre-production planning to
               shooting, to post-production editing and retouching, we ensure
@@ -28,10 +28,10 @@ function Services() {
           <hr className="border-black " />
           <section
             id="videography"
-            className="w-full mt-20 md:mt-28 mb-20 flex flex-col md:flex-row justify-between"
+            className="w-full mt-16 md:mt-28 mb-20 flex flex-col md:flex-row justify-between"
           >
-            <h3 className="text-5xl md:w-1/2">Videography</h3>
-            <p className="text-xl mt-12 md:w-1/2">
+            <h3 className="text-4xl md:text-5xl md:w-1/2">Videography</h3>
+            <p className="text-lg md:text-xl mt-8 md:mt-12 md:w-1/2">
               Having quality video content is important for your business,
               whether it's a simple how-to video or a more impactful piece for
               social media. That's why we offer comprehensive services from
@@ -42,10 +42,10 @@ function Services() {
           <hr className="border-black " />
           <section
             id="set-design"
-            className="w-full mt-20 lg:mt-28 mb-8 md:mb-16 lg:mb-20 flex flex-col md:flex-row justify-between"
+            className="w-full mt-16 lg:mt-28 mb-8 md:mb-16 lg:mb-20 flex flex-col md:flex-row justify-between"
           >
-            <h3 className="text-5xl md:w-1/2">Set Design</h3>
-            <p className="text-xl mt-12 md:w-1/2">
+            <h3 className="text-4xl md:text-5xl md:w-1/2">Set Design</h3>
+            <p className="text-lg md:text-xl mt-8 md:mt-12 md:w-1/2">
               We create stunning sets using the latest techniques and
               technology. Whether you need a traditional set, digital elements,
               or 3D printed materials, we've got you covered.
