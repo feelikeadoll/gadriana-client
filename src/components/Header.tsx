60a8f29e969fc87e2ddb032fa1fa70178af4e1ea
Laia Navalon <laianavalon@gmail.com>
2024-07-31 17:45:06 +0200

    Change social media icons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -42,16 +42,11 @@ function Header() {
               className="hidden md:block ml-10"
             >
               <svg
-                width="36px"
-                height="36px"
-                viewBox="0 0 48 48"
-                version="1.1"
-                id="Shopicons"
+                width="38px"
+                height="38px"
+                viewBox="0 0 24 24"
+                fill="none"
                 xmlns="http://www.w3.org/2000/svg"
-                x="0"
-                y="0"
-                white-space="preserve"
-                fill="#000000"
               >
                 <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
                 <g
@@ -60,12 +55,13 @@ function Header() {
                   strokeLinejoin="round"
                 ></g>
                 <g id="SVGRepo_iconCarrier">
-                  <g id="instagram_00000127030415805702900630000013396224003006281109_">
-                    <path d="M0-.019h48v48H0v-48z" fill="none"></path>
-                    <path d="M36.019 8A3.986 3.986 0 0 1 40 11.981v24.038A3.986 3.986 0 0 1 36.019 40H11.981A3.986 3.986 0 0 1 8 36.019V11.981A3.986 3.986 0 0 1 11.981 8h24.038m0-4H11.981A7.98 7.98 0 0 0 4 11.981v24.038A7.98 7.98 0 0 0 11.981 44h24.038A7.98 7.98 0 0 0 44 36.019V11.981A7.98 7.98 0 0 0 36.019 4z"></path>
-                    <path d="M24 34c-5.514 0-10-4.486-10-10s4.486-10 10-10 10 4.486 10 10-4.486 10-10 10zm0-16c-3.309 0-6 2.691-6 6s2.691 6 6 6 6-2.691 6-6-2.691-6-6-6z"></path>
-                    <circle cx="35" cy="13" r="2"></circle>
-                  </g>
+                  <path
+                    d="M16.65 7.2H16.66M8 20H16C18.2091 20 20 18.2091 20 16V8C20 5.79086 18.2091 4 16 4H8C5.79086 4 4 5.79086 4 8V16C4 18.2091 5.79086 20 8 20ZM15.75 12C15.75 14.0711 14.0711 15.75 12 15.75C9.92893 15.75 8.25 14.0711 8.25 12C8.25 9.92893 9.92893 8.25 12 8.25C14.0711 8.25 15.75 9.92893 15.75 12Z"
+                    stroke="#000000"
+                    strokeWidth="1.6799999999999997"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  ></path>
                 </g>
               </svg>
             </a>
