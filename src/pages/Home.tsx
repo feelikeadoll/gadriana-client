b18b8f5ccc93f592b775622b83d03cd799d88dae
Laia Navalon <laianavalon@gmail.com>
2024-09-03 19:25:44 +0200

    Adjust design for smaller screens

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -36,9 +36,8 @@ function Home() {
   return (
     <div className="w-full">
       <div className="pt-20 bg-homeGif w-full h-96 md:h-screen bg-cover bg-center">
-        <h1 className="mx-12 pt-40 text-3.5xl md:text-4xl md:w-11/12 md:w-3/4 lg:w-2/3">
-          We're a creative studio
-          <br />
+        <h1 className="w-1/2 mx-6 md:mx-12 pt-20 md:pt-32 lg:pt-40 text-2xl md:text-3.5xl lg:text-4xl md:w-11/12 md:w-3/4 lg:w-2/3">
+          We're a creative studio <br className="hidden md:block" />
           for beauty + wellness brands.
         </h1>
       </div>
@@ -46,10 +45,11 @@ function Home() {
 
       <div className="px-6 flex justify-between items-center mx-auto">
         <div className="flex flex-col items-center mb-32">
-          <h2 className="w-7/12 text-3xl text-center my-32">
-            We create stunning imagery and videos <br />
-            that help our clients captivate their audience
-            <br />
+          <h2 className="w-5/6 sm:w-4/6 lg:w-7/12 text-2xl md:text-3xl text-center my-16 md:my-20 lg:my-32">
+            We create stunning imagery and videos{" "}
+            <br className="hidden lg:block" />
+            that help our clients captivate their audience{" "}
+            <br className="hidden xl:block" />
             in a visually impactful manner.
           </h2>
           <div className="w-full flex justify-center">
