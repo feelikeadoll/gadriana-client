2352fc3343e1f9bc425401b91640cdae60bd5088
Laia Navalon <laianavalon@gmail.com>
2024-09-03 18:31:53 +0200

    Delete About page as requested by client and remove it from routing in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import About from "./pages/About";
 import Services from "./pages/Services";
 import BackToTopButton from "./components/BackToTopButton";
 import ContactSection from "./components/ContactSection";
@@ -15,7 +14,6 @@ function App() {
 
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
         <Route path="/services" element={<Services />} />
       </Routes>
 
