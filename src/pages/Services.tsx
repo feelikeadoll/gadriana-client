74f327caa3eeb25299ce12e85c3c223df34953ed
Laia Navalon <laianavalon@gmail.com>
2024-09-04 13:14:33 +0200

    Adjust initial text in Services.tsx for responsiveness

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -24,19 +24,24 @@ function Services() {
             id="about"
             className="px-6 flex justify-between items-center mx-auto"
           >
-            <div className="flex-col mt-32 mb-16 w-full">
+            <div className="flex-col mt-10 md:mt-32 mb-16 w-full">
               <div className="w-full flex flex-col items-center">
                 <div className="w-full md:w-11/12 lg:w-3/4 text-3xl md:text-3.5xl lg:text-4xl leading-relaxed">
-                  <p className="pt-8">
+                  <p className="pt-8 hidden md:block">
                     <img
                       src={HelloImg}
-                      className="w-52 mb-12 mr-4 inline-block"
+                      className="h-12 md:h-16 mb-8 md:mb-12 mr-4 md:inline-block"
                       alt=""
                     />
                     We're a creative studio
                   </p>
-                  <p className="-mt-8">
+                  <p className="md:-mt-8 hidden md:block">
                     based in Barcelona for beauty + wellness brands.
+                  </p>
+                  <p className="pt-8 block md:hidden">
+                    <img src={HelloImg} className="h-12 mb-8 mr-4" alt="" />
+                    We're a creative studio based in Barcelona for beauty +
+                    wellness brands.
                   </p>
                   <p className="py-8">
                     We direct, create, style, build, style, retouch and perfect
