96ef17ca712f6938452eeb5628ba31423fb359db
Laia Navalon <laianavalon@gmail.com>
2024-09-04 13:51:24 +0200

    Adjust design for smaller screens

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -10,22 +10,30 @@ function ContactSection() {
     >
       <div className="w-full flex flex-col items-center justify-center">
         <hr className="border-black border-t-1 w-full" />
-        <div className="w-full px-6 pt-28 pb-14 flex justify-between">
-          <div className="w-1/2 text-lg md:text-xl">
+        <div className="w-full px-6 pt-28 pb-14 flex flex-col md:flex-row justify-between">
+          <div className="mb-10 md:w-1/2 text-lg md:text-xl">
             <img
               src={Collaborate}
               alt="Gadriana Studio Barcelona - Get in touch via Contact form"
               className="w-72 pb-4"
             />
-            <p className="mb-2">
-              Have a project in mind? <br />
-              Fill out the form or e-mail us at
+            <div className="hidden md:block">
+              <p className="md:mb-2">
+                Have a project in mind? <br />
+                Fill out the form or e-mail us at
+              </p>
+              <a href="mailto:studio@gadriana.com" className="underline">
+                studio@gadriana.com
+              </a>
+            </div>
+            <p className="block md:hidden mt-2">
+              Have a project in mind? Fill out the form or e-mail us at{" "}
+              <a href="mailto:studio@gadriana.com" className="underline">
+                studio@gadriana.com
+              </a>
             </p>
-            <a href="mailto:studio@gadriana.com" className="underline">
-              studio@gadriana.com
-            </a>
           </div>
-          <div className="w-1/2">
+          <div className="md:w-1/2">
             <ContactForm />
           </div>
         </div>
