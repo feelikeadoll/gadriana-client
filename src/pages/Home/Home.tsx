d501284493c67afa7c9f3dd0117e6dd4511fce5b
Laia Navalon Arxe <laia.navalon@radissonhotels.com>
2026-06-28 19:57:27 +0200

    fix category/brand select

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -5,64 +5,86 @@ function Home() {
   const [selectedCategory, setSelectedCategory] = useState<string>("all");
   const [selectedBrand, setSelectedBrand] = useState<string>("all");
 
+  const handleCategoryClick = (category: string) => {
+    setSelectedBrand("all");
+    setSelectedCategory((prev) => (prev === category ? "all" : category));
+  };
+
+  const handleBrandClick = (brand: string) => {
+    setSelectedCategory("all");
+    setSelectedBrand((prev) => (prev === brand ? "all" : brand));
+  };
+
   return (
     <div className="w-full">
       <div className="px-8 flex justify-between items-center mx-auto">
         <div className="flex flex-col items-center mb-32 w-full">
           <div className="mt-4 w-full h-8 flex justify-end items-center gap-x-4 gap-y-0 tracking-wide">
-            <button 
-              onClick={() => setSelectedCategory(prev => prev === "Still" ? "all" : "Still")}
-              className={selectedCategory === "Still" ? "font-semibold" : "font-normal"}
-            >
+            <button
+              onClick={() => handleCategoryClick("Still")}
+              className={
+                selectedCategory === "Still" ? "font-semibold" : "font-normal"
+              }>
               STILL LIFE
             </button>
             <div> | </div>
-            <button 
-              onClick={() => setSelectedCategory(prev => prev === "Texture" ? "all" : "Texture")}
-              className={selectedCategory === "Texture" ? "font-semibold" : "font-normal"}
-            >
+            <button
+              onClick={() => handleCategoryClick("Texture")}
+              className={
+                selectedCategory === "Texture" ? "font-semibold" : "font-normal"
+              }>
               TEXTURE
             </button>
             <div> | </div>
-            <button 
-              onClick={() => setSelectedCategory(prev => prev === "Hair & Skin" ? "all" : "Hair & Skin")}
-              className={selectedCategory === "Hair & Skin" ? "font-semibold" : "font-normal"}
-            >
+            <button
+              onClick={() => handleCategoryClick("Hair & Skin")}
+              className={
+                selectedCategory === "Hair & Skin"
+                  ? "font-semibold"
+                  : "font-normal"
+              }>
               HAIR & SKIN
             </button>
             <div> | </div>
-            <button 
-              onClick={() => setSelectedCategory(prev => prev === "Motion" ? "all" : "Motion")}
-              className={selectedCategory === "Motion" ? "font-semibold" : "font-normal"}
-            >
+            <button
+              onClick={() => handleCategoryClick("Motion")}
+              className={
+                selectedCategory === "Motion" ? "font-semibold" : "font-normal"
+              }>
               MOTION
             </button>
           </div>
 
           <div className="w-full h-8 mb-12 flex justify-end items-center gap-x-4 gap-y-0 tracking-wide">
-             <button 
-              onClick={() => setSelectedBrand(prev => prev === "Loving Tan" ? "all" : "Loving Tan")}
-              className={selectedBrand === "Loving Tan" ? "font-semibold" : "font-normal"}
-            >
+            <button
+              onClick={() => handleBrandClick("Loving Tan")}
+              className={
+                selectedBrand === "Loving Tan" ? "font-semibold" : "font-normal"
+              }>
               LOVING TAN
             </button>
             <div> | </div>
-            <button 
-              onClick={() => setSelectedBrand(prev => prev === "Dcypher" ? "all" : "Dcypher")}
-              className={selectedBrand === "Dcypher" ? "font-semibold" : "font-normal"}
-            >
+            <button
+              onClick={() => handleBrandClick("Dcypher")}
+              className={
+                selectedBrand === "Dcypher" ? "font-semibold" : "font-normal"
+              }>
               DCYPHER
             </button>
             <div> | </div>
-            <button 
-              onClick={() => setSelectedBrand(prev => prev === "Coco & Eve" ? "all" : "Coco & Eve")}
-              className={selectedBrand === "Coco & Eve" ? "font-semibold" : "font-normal"}
-            >
+            <button
+              onClick={() => handleBrandClick("Coco & Eve")}
+              className={
+                selectedBrand === "Coco & Eve" ? "font-semibold" : "font-normal"
+              }>
               COCO & EVE
             </button>
           </div>
 
-          <HomeImgGrid selectedCategory={selectedCategory} selectedBrand={selectedBrand}/>
+          <HomeImgGrid
+            selectedCategory={selectedCategory}
+            selectedBrand={selectedBrand}
+          />
         </div>
       </div>
     </div>
