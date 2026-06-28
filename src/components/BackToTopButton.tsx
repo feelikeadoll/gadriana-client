ff47d8ee9da1e92ad1209a41b1eb45cb2b5fe407
Laia Navalon Arxe <laia.navalon@radissonhotels.com>
2026-06-28 19:41:07 +0200

    change style of BackToTop button

--- src/components/BackToTopButton.tsx
+++ src/components/BackToTopButton.tsx
@@ -21,43 +21,25 @@ function BackToTopButton() {
       {backToTop && (
         <button
           onClick={scrollBack}
-          className="shadow-around border-1 border-black z-45 fixed bottom-12 right-12 md:bottom-16 md:right-16 lg:bottom-20 lg:right-20 btn btn-circle btn-lg bg-white"
-        >
+          className="shadow-around z-45 fixed bottom-12 right-12 md:bottom-16 md:right-16 lg:bottom-20 lg:right-20 btn btn-circle btn-lg bg-white">
           <svg
-            width="50px"
-            height="50px"
-            viewBox="0 0 24 24"
-            fill="none"
+            fill="#211f20"
+            height="27px"
+            width="27px"
+            version="1.1"
+            id="Layer_1"
             xmlns="http://www.w3.org/2000/svg"
-          >
+            xmlnsXlink="http://www.w3.org/1999/xlink"
+            viewBox="0 0 512 512"
+            enableBackground="new 0 0 512 512"
+            xmlSpace="preserve">
             <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
             <g
               id="SVGRepo_tracerCarrier"
               strokeLinecap="round"
-              strokeLinejoin="round"
-            ></g>
+              strokeLinejoin="round"></g>
             <g id="SVGRepo_iconCarrier">
-              <g clipPath="url(#clip0_429_11224)">
-                <path
-                  d="M17 14L12 9"
-                  stroke="#000000"
-                  strokeWidth="2.5"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                ></path>
-                <path
-                  d="M12 9L7 14"
-                  stroke="#000000"
-                  strokeWidth="2.5"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                ></path>
-              </g>
-              <defs>
-                <clipPath id="clip0_429_11224">
-                  <rect width="24" height="24" fill="white"></rect>
-                </clipPath>
-              </defs>
+              <polygon points="256,63.6 0,319.6 69.8,389.4 256,203.2 442.2,389.4 512,319.6 "></polygon>
             </g>
           </svg>
         </button>
